5da3daf5b76b4e19b97e5b46997649a2513160a7
Ella.Yoon <Ella.Yoon@advantech.com>
2024-03-15 17:07:06 +0900

    .

--- drive4u/App.js
+++ drive4u/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>민선아 나 일하기 싫어</Text>
       <StatusBar style="auto" />
     </View>
   );
